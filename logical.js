8cc3658d510116e88f4b9888920aa0c01808c1cd
Kleito23 <124531090+Kleito23@users.noreply.github.com>
2023-04-14 20:59:47 -0500

    Utlima version

---
+++ logical.js
@@ -0,0 +1,25 @@
+
+function encriptar(){
+    document.getElementById("imagen").style.display = "none";
+    var texto = document.getElementById("texto-a-modificar").value;
+    var texto_encriptado = ""
+    for (var i = 0; i < texto.length; i++){
+        var caracter = texto.charAt(i);
+        if (caracter == "a"){
+            texto_encriptado += "ai";
+        }else if (caracter == "e"){
+            texto_encriptado += "enter";
+        }else if (caracter == "i"){
+            texto_encriptado += "imes";
+        }else if (caracter == "o"){
+            texto_encriptado += "ober";
+        }else if (caracter == "u"){
+            texto_encriptado += "ufat";
+        }else{
+            texto_encriptado += caracter;
+        }
+
+    document.getElementById("texto-modificado").innerHTML = "<p>" + texto_encriptado + "</p>";
+    }
+
+}
